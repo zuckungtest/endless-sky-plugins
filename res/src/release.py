14bdd0b46d3924a934f87c4925472d9d65e773d9
zuckungtest <zuckung@quantentunnel.de>
2025-04-18 08:58:07 +0200

    Update release.py

--- res/src/release.py
+++ res/src/release.py
@@ -68,7 +68,6 @@ def write_news(p, inputnews):
 	today = datetime.today().strftime('%Y-%m-%d')
 	with open('res/news.txt') as newsfile:
 		old = newsfile.readlines()
-	print(inputnews)
 	if inputnews == 'x':
 		news = ''
 	else:
