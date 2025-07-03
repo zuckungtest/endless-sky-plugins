6c5c8b2839e5448222c563027b1cfe1dc17044c3
zuckungtest <zuckung@quantentunnel.de>
2025-07-03 19:07:46 +0200

    Update getversion.py

--- res/src/getversion.py
+++ res/src/getversion.py
@@ -7,15 +7,15 @@ def get_version():
 	with open('changelog.txt', 'wb') as changelog:
 		changelog.write(request.content) # downloading the changelog
 	with open('changelog.txt', 'r') as sourcefile:
-		onlineversion = sourcefile.readline().replace('Version ', '').replace('\n', '') # result example: 0.10.10
-	if int(onlineversion[-1])%2 == 0:
-		versiontag = '"v' + onlineversion + ': Stable Release"'
-	else:
-		versiontag = '"v' + onlineversion + ': Unstable Release"'
-	releasev = 'EndlessSky-v' + onlineversion.replace('"', '') + '-win64-setup.exe '
+		onlineversion = 'v' + sourcefile.readline().replace('Version ', '').replace('\n', '') # result example: 0.10.10
+#	if int(onlineversion[-1])%2 == 0:
+#		versiontag = '"v' + onlineversion + ': Stable Release"'
+#	else:
+#		versiontag = '"v' + onlineversion + ': Unstable Release"'
+	releasev = 'EndlessSky-' + onlineversion + '-win64-setup.exe '
 	env_file = os.getenv('GITHUB_ENV')
 	with open(env_file, "a") as myfile:
-		myfile.write("ES_VERSION=" + versiontag + '\n')
+		myfile.write("ES_VERSION=" + onlineversion + '\n')
 		myfile.write("RELEASE=" + releasev + '\n')
 
 
