876e63e77246d39ad55126d52ea6a7a9d25d0ded
zuckungtest <zuckung@quantentunnel.de>
2025-07-03 18:45:47 +0200

    Add files via upload

--- res/src/getversion.py
+++ res/src/getversion.py
@@ -8,9 +8,13 @@ def get_version():
 		changelog.write(request.content) # downloading the changelog
 	with open('changelog.txt', 'r') as sourcefile:
 		onlineversion = sourcefile.readline().replace('Version ', '').replace('\n', '') # result example: 0.10.10
+	if int(onlineversion[-1])%2 == 0:
+		versiontag = 'v' + onlineversion + ': Stable Release'
+	else:
+		versiontag = 'v' + onlineversion + ': Unstable Release'
 	env_file = os.getenv('GITHUB_ENV')
 	with open(env_file, "a") as myfile:
-		myfile.write("ES_VERSION=" + onlineversion + '\n')
+		myfile.write("ES_VERSION=" + versiontag + '\n')
 
 
 def run():
