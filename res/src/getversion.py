30696c05d76fb98cd479ad8419d72d0b4f5e2602
zuckungtest <zuckung@quantentunnel.de>
2025-07-03 18:24:35 +0200

    Add files via upload

---
+++ res/src/getversion.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+
+def get_version():
+	request = requests.get('https://github.com/endless-sky/endless-sky/raw/refs/heads/master/changelog')
+	with open('changelog.txt', 'wb') as changelog:
+		changelog.write(request.content) # downloading the changelog
+	with open('changelog.txt', 'r') as sourcefile:
+		onlineversion = sourcefile.readline().replace('Version ', '').replace('\n', '') # result example: 0.10.10
+	env_file = os.getenv('GITHUB_ENV')
+	with open(env_file, "a") as myfile:
+		myfile.write("ES_VERSION=" + onlineversion + '\n')
+
+
+def run():
+	get_version()
+
+
+if __name__ == "__main__":
+	run()
